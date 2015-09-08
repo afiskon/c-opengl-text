03763d08385510db5f42486c0031aeaed66b7725
afiskon <afiskon@gmail.com>
2015-09-08 09:39:56 +0300

    Rotation implemented

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <GLXW/glxw.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/transform.hpp>
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <vector>
 #include <defer.h>
+#include <chrono>
 #include "utils/utils.h"
 
 static const GLfloat globVertexBufferData[] = {
@@ -95,7 +97,37 @@ int main() {
   glBindBuffer(GL_ARRAY_BUFFER, 0); // unbind VBO
   glBindVertexArray(0); // unbind VAO
 
+
+  // Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+  glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 1.0f, 100.0f);
+  // Camera matrix
+  glm::mat4 View       = glm::lookAt(
+      //glm::vec3(4,3,3), // Camera is at (4,3,3), in World Space
+      glm::vec3(0, 0, 5),
+      glm::vec3(0, 0, 0), // and looks at the origin
+      glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
+  );
+  // Model matrix : an identity matrix (model will be at the origin)
+//  glm::mat4 Model      = glm::rotate(30.0f, 0.0f, 1.0f, 0.0f); // glm::mat4(1.0f);  // Changes for each model !
+  // Our ModelViewProjection : multiplication of our 3 matrices
+//  glm::mat4 MVP        = Projection * View * Model; // Remember, matrix multiplication is the other way around
+
+  // Get a handle for our "MVP" uniform.
+  // Only at initialisation time.
+  GLint matrixId = glGetUniformLocation(programId, "MVP");
+
+  auto startTime = std::chrono::high_resolution_clock::now();
   while(glfwWindowShouldClose(window) == GL_FALSE) {
+    auto currentTime = std::chrono::high_resolution_clock::now();
+    auto duration = currentTime - startTime;
+    long ms = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
+    long rotationTimeMs = 3000;
+    float angle = (360.0f/rotationTimeMs) * ms;
+
+    glm::mat4 Model      = glm::rotate(angle, 0.0f, 1.0f, 0.0f);
+    glm::mat4 MVP        = Projection * View * Model; // Remember, matrix multiplication is the other way around
+    glUniformMatrix4fv(matrixId, 1, GL_FALSE, &MVP[0][0]);
+
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glUseProgram(programId);
--- shaders/vertexShader.glsl
+++ shaders/vertexShader.glsl
@@ -1,8 +1,9 @@
 #version 330 core
 
 layout(location = 0) in vec3 vertexPos;
+uniform mat4 MVP;
 
 void main() {
-  gl_Position.xyz = vertexPos;
-  gl_Position.w = 1.0;
+  vec4 temp = vec4(vertexPos, 1);
+  gl_Position = MVP * temp;
 }
