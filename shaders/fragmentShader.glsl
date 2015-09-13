4ddae63a0ba5da4dcbbc9eb5212f9d4a976bf009
afiskon <afiskon@gmail.com>
2015-09-13 18:18:50 +0300

    Minor changes

--- shaders/fragmentShader.glsl
+++ shaders/fragmentShader.glsl
@@ -1,6 +1,5 @@
 #version 330 core
 
-// in vec3 fragmentColor;
 in vec2 UV;
 
 uniform sampler2D textureSampler;
@@ -8,6 +7,5 @@ uniform sampler2D textureSampler;
 out vec3 color;
 
 void main() {
-//  color = fragmentColor;
   color = texture(textureSampler, UV).rgb;
 }
--- shaders/vertexShader.glsl
+++ shaders/vertexShader.glsl
@@ -1,17 +1,13 @@
 #version 330 core
 
 layout(location = 0) in vec3 vertexPos;
-//layout(location = 1) in vec3 vertexColor;
 layout(location = 1) in vec2 vertexUV;
-
-out vec2 UV;
-//out vec3 fragmentColor;
 
 uniform mat4 MVP;
 
-void main() {
-//  fragmentColor = vertexColor;
-  UV = vertexUV;
+out vec2 UV;
 
+void main() {
+  UV = vertexUV;
   gl_Position = MVP * vec4(vertexPos, 1);
 }
