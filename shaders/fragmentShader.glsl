48e6332fea3b4880f9a2970597f163f317b01668
afiskon <afiskon@gmail.com>
2015-10-01 10:46:03 +0300

    Work in progress

--- shaders/fragmentShader.glsl
+++ shaders/fragmentShader.glsl
@@ -7,5 +7,5 @@ uniform sampler2D textureSampler;
 out vec3 color;
 
 void main() {
-  color = vec3(0.5f,0.5f,0.5f) * texture(textureSampler, UV).rgb;
+  color = vec3(1.0f,1.0f,1.0f) * texture(textureSampler, UV).rgb;
 }
